240f0598857421856abd6c7b80797d3f46589a7f
whale-ao <wtao.roa@gmail.com>
2021-07-04 13:01:40 -0500

    bunted

--- DMTS new.py
+++ DMTS new.py
@@ -9,8 +9,9 @@ from functools import partial  # for passing arguments to events handled within 
 # Global Variables
 # ---------------------
 VI_list = [10, 12, 12, 15, 15, 15, 17, 17, 20]  # varying break intervals (VI: variable interval)
-# ---------------------
 
+
+# ---------------------
 
 
 # Setup I/Os.
@@ -68,8 +69,10 @@ def stage1():
     sample_btn1 = tk.Button(gui, image=correct, command=stage2())
     sample_btn1.place(relheight=0.4, rely=0.3, relwidth=0.3, relx=0.65, anchor="ne")
 
+
 def stage2():
     print('bunted')
+
 
 if __name__ == '__main__':
     # GUI Initialization
@@ -90,9 +93,8 @@ if __name__ == '__main__':
         # converts the image into a compatible tkinter image
         imglist.append(ImageTk.PhotoImage(imgtemp))
 
-
     # local variation of the press function to handle pushes of the start and stop button in the main menu
-    print(VI_list)
+
     def press(btn):
         if btn == "START":
             stage1()
@@ -100,17 +102,26 @@ if __name__ == '__main__':
         else:
             gui.destroy()
             sys.exit()
-    #upon push of start, call this function to remove the start and quit buttons
+
     def main_menu_destroy():
         start_button.destroy()
         quit_button.destroy()
 
+    # upon push of start, call this function to remove the start and quit buttons
+
+    # THESE ARE GLOBAL SO THE press(btn) FUNCTION IS ABLE TO HANDLE BUTTON PRESSES
+    # this press(btn) function is specific to this segment of code because it is only handling
+    # main menu inputs
+    global start_button, quit_button
     # creates and places a button with the text START which begins stage1()
-    start_button = tk.Button(gui, text="START", font=("bold", "40"), command=partial(press,"START")) \
+    start_button = tk.Button(gui, text="START", font=("bold", "40"), command=partial(press, "START")) \
         .place(relheight=0.5, relwidth=0.5, relx=1, rely=0, anchor="ne")
 
     # creates and places a button with the text QUIT which destroys the gui and exits the program
     quit_button = tk.Button(gui, text="QUIT", font=("bold", "40"), command=partial(press, "QUIT")) \
         .place(relheight=.15, relwidth=.1, relx=1, rely=.85, anchor="ne")
+
+
+
 
     gui.mainloop()
